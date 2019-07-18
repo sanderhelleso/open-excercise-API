e7fb4dae5195f40e35f7fce6960497400cfca083
Tarald Johansen <tarald.johansen@outlook.com>
2019-07-19 01:06:42 +0200

    Installed Ant Design for React

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import "./App.css";
+import Button from "antd/es/button";
 
 const App: React.FC = () => {
     return (
         <div className="App">
             <h1>Open Excercise API</h1>
+            <Button type="primary">Click Me!</Button>
         </div>
     );
 };
