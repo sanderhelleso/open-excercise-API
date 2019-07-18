01d3cc270ee86eb2057ed2cffa6a33ee7e79a262
Tarald Johansen <tarald.johansen@outlook.com>
2019-07-19 00:44:15 +0200

    Install React App with Typescript/TSLint

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <h1>Open Excercise API</h1>
+        </div>
+    );
+};
+
+export default App;
