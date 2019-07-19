adff0ba040c6609b3a880d1eb834f309330d861a
Sanderhelleso <sanderhelleso@hotmail.com>
2019-07-19 22:35:24 +0200

    Fixed import bug

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ExcercisesController } from './excercises/excercises.controller';
 import { ExcercisesController } from './controllers/excercises/excercises.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ExcercisesController],
-  providers: [AppService],
+	imports: [],
+	controllers: [ AppController, ExcercisesController ],
+	providers: [ AppService ]
 })
 export class AppModule {}
