84800aef87c7580b7899a3b3bcc626f9bb364d70
Sanderhelleso <sanderhelleso@hotmail.com>
2019-07-22 00:21:01 +0200

    Added customers service

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,7 +3,6 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth.module';
 import { QuotasModule } from './modules/quotas.module';
-import { ApiController } from './controllers/api/api.controller';
 import { ApiModule } from './modules/api.module';
 
 @Module({
