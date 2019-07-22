052c70a2687085b3fe96383fb1d016136a02f9ea
Sanderhelleso <sanderhelleso@hotmail.com>
2019-07-22 22:44:32 +0200

    Update analytics.gateway.ts

--- server/src/analytics.gateway.ts
+++ server/src/analytics.gateway.ts
@@ -4,17 +4,17 @@ import { WebSocketGateway, SubscribeMessage, OnGatewayDisconnect, WebSocketServe
 export class AnalyticsGateway implements OnGatewayDisconnect {
 	private activeClients: Map<string, any> = new Map<string, any>();
 
+	@SubscribeMessage('recieve_user_id')
+	handleRecieveUserID(client: any, payload: any) {
+		this.activeClients.set(payload, client);
+	}
+
 	handleDisconnect(client: any) {
 		this.activeClients.forEach((_client, key) => {
 			if (client === _client) {
 				this.activeClients.delete(key);
 			}
 		});
-	}
-
-	@SubscribeMessage('recieve_user_id')
-	handleRecieveUserID(client: any, payload: any) {
-		this.activeClients.set(payload, client);
 	}
 
 	emitAnalyticsPayload(userID: string, payload: any): any {
