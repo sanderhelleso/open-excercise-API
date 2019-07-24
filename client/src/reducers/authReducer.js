d26456feace6a9b17904107cfe16ef47ae617d49
Tarald Johansen <tarald.johansen@outlook.com>
2019-07-25 00:42:39 +0200

    Added login case to authReducer and login type

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,19 +1,24 @@
-import { REGISTER } from '../actions/types'
+import { REGISTER, LOGIN } from "../actions/types";
 
 const initialState = {
     isAuthenticated: null,
     token: null
-}
+};
 
 export default (state = initialState, aciton) => {
-    switch(aciton.type) {
+    switch (aciton.type) {
         case REGISTER:
-            return{
+            return {
                 ...state,
                 ...aciton.payload,
-                isAuthenticated: true,
-            }
+                isAuthenticated: true
+            };
+        case LOGIN:
+            return {
+                ...state,
+                isAuthenticated: true
+            };
         default:
-            return state
+            return state;
     }
-}
+};
