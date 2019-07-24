2ec9160b874e21bc94685ead161fa3c7477f7976
Tarald Johansen <tarald.johansen@outlook.com>
2019-07-25 01:13:55 +0200

    HOTFIX: Updated Router

--- client/src/components/routes/Router.js
+++ client/src/components/routes/Router.js
@@ -2,14 +2,33 @@ import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Register from "../register/Register";
 import SocketTest from "../socket-test/SocketTest";
+import Login from "../login/Login";
 import { connect } from "react-redux";
-import Dashboard from '../dashboard/Dashboard';
+import Dashboard from "../dashboard/Dashboard";
 
 const Router = ({ isAuthenticated }) => {
     return (
         <BrowserRouter>
             <Switch>
-      				<Route exact path="/dashboard" component={Dashboard} />
+                <Route exact path="/dashboard" component={Dashboard} />
+                <Route
+                    exact
+                    path="/"
+                    component={
+                        isAuthenticated
+                            ? () => <Redirect to="/dashboard" />
+                            : Login
+                    }
+                />
+                <Route
+                    exact
+                    path="/login"
+                    component={
+                        isAuthenticated
+                            ? () => <Redirect to="/dashboard" />
+                            : Login
+                    }
+                />
                 <Route exact path="/socket-test" component={SocketTest} />
                 <Route
                     exact
