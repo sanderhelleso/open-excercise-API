3536c88bc27ebae7c51fc0bbe276787fb4071044
Sanderhelleso <sanderhelleso@hotmail.com>
2019-07-25 02:10:47 +0200

    create_initial_dashboard_layout: deleted unneeded files

--- client/src/components/dashboard/apiKey/ApiKey.js
+++ client/src/components/dashboard/apiKey/ApiKey.js
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import ApiKeyField from './ApiKeyField';
+import ApiKeyUpdateBtn from './ApiKeyUpdateBtn';
 
 const ApiKey = () => {
+	const [ blur, setBlur ] = useState(true);
+
 	return (
-		<StyledDiv>
+		<StyledCont>
 			<h5>My API key</h5>
-			<ApiKeyField />
-		</StyledDiv>
+
+			<StyledDiv blur={blur}>
+				<span>a1954334128f525582e4280f006321708475b17d75aeeddc67db5f09a98b1546</span>
+				<ApiKeyUpdateBtn />
+			</StyledDiv>
+		</StyledCont>
 	);
 };
 
 export default ApiKey;
 
-const StyledDiv = styled.div`
+const StyledCont = styled.div`
 	margin-top: 2rem;
 
 	h5 {
 		font-size: 1rem;
 		margin-bottom: 1.5rem;
+	}
+`;
+
+const StyledDiv = styled.div`
+	padding: 12.5px 12.5px;
+	padding-right: 2.5rem;
+	background-color: #fefefe;
+	height: 50px;
+	border-radius: 100px;
+	display: flex;
+	align-items: center;
+	position: relative;
+	margin-right: 1rem;
+	border: 1.25px solid #eeeeee;
+
+	span {
+		font-weight: 400;
+		margin-right: 2rem;
+		filter: ${({ blur }) => (blur ? 'blur(3px)' : 'blur(0)')};
 	}
 `;
