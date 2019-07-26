66a42d5a6bee67054880718ce1379e45ed779034
Sanderhelleso <sanderhelleso@hotmail.com>
2019-07-26 04:35:02 +0200

    implement_analytics_chart: added chart styles

--- client/src/components/dashboard/chart/Chart.js
+++ client/src/components/dashboard/chart/Chart.js
@@ -1,63 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
-import { LineChart, Line, CartesianGrid, YAxis, XAxis, ResponsiveContainer } from 'recharts';
+import { AreaChart, Area, Line, YAxis, XAxis, Legend, ResponsiveContainer, Tooltip } from 'recharts';
 
-const data = [
-	{
-		name: 'Page A',
-		uv: 4000,
-		pv: 2400,
-		amt: 2400
-	},
-	{
-		name: 'Page B',
-		uv: 3000,
-		pv: 1398,
-		amt: 2210
-	},
-	{
-		name: 'Page C',
-		uv: 2000,
-		pv: 9800,
-		amt: 2290
-	},
-	{
-		name: 'Page D',
-		uv: 2780,
-		pv: 3908,
-		amt: 2000
-	},
-	{
-		name: 'Page E',
-		uv: 1890,
-		pv: 4800,
-		amt: 2181
-	},
-	{
-		name: 'Page F',
-		uv: 2390,
-		pv: 3800,
-		amt: 2500
-	},
-	{
-		name: 'Page G',
-		uv: 3490,
-		pv: 4300,
-		amt: 2100
-	}
-];
+const generateData = () => {
+	const data = Array(6).fill(0).map((_, i) => ({
+		Period: 'Period ' + i,
+		Requests: Math.floor(Math.random() * 10) + 2,
+		amt: Math.floor(Math.random() * 10) + 2
+	}));
+
+	return data;
+};
 
 const Chart = () => {
 	return (
 		<StyledChartCont>
 			<ResponsiveContainer width="100%">
-				<LineChart data={data}>
-					<XAxis dataKey="name" />
-					<YAxis />
-					<CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-					<Line type="monotone" dataKey="uv" stroke="#8884d8" />
-					<Line type="monotone" dataKey="pv" stroke="#82ca9d" />
-				</LineChart>
+				<AreaChart data={generateData()}>
+					<defs>
+						<linearGradient id="colorRequests" x1="0" y1="0" x2="0" y2="1">
+							<stop offset="5%" stopColor="#139ff2" stopOpacity={0.3} />
+							<stop offset="95%" stopColor="#139ff2" stopOpacity={0} />
+						</linearGradient>
+					</defs>
+					<XAxis dataKey="Period" dy={15} stroke="#e0e0e0" tick={{ fill: '#9e9e9e', fontWeight: '100' }} />
+					<YAxis dx={-15} stroke="#e0e0e0" tick={{ fill: '#9e9e9e', fontWeight: '100' }} />
+					<Tooltip />
+					<Legend
+						verticalAlign="top"
+						align="right"
+						wrapperStyle={{
+							fontWeight: '100',
+							marginBottom: '25px',
+							paddingBottom: '25px',
+							color: '#9e9e9e'
+						}}
+					/>
+					<Line type="monotone" dataKey="Requests" stroke="#139ff2" activeDot={{ r: 6 }} />
+					<Area
+						type="monotone"
+						dataKey="Requests"
+						stroke="#139ff2"
+						fillOpacity={1}
+						fill="url(#colorRequests)"
+					/>
+				</AreaChart>
 			</ResponsiveContainer>
 		</StyledChartCont>
 	);
@@ -67,5 +54,14 @@ export default Chart;
 
 const StyledChartCont = styled.div`
 	max-height: 350px;
-	margin-top: 2rem;
+	margin-top: 1rem;
+	font-weight: 400;
+	font-size: 0.9rem;
+	background-color: #fefefe;
+	border: 1.25px solid #eeeeee;
+	border-radius: 4px;
+	box-shadow: 0px 7.5px 15px rgba(0, 0, 0, 0.045);
+	padding: 1.5rem;
+	padding-right: 2.5rem;
+	padding-bottom: 2.5rem;
 `;
