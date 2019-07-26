feb8973d9f5f198726990174c678a644c968330f
Sanderhelleso <sanderhelleso@hotmail.com>
2019-07-26 02:30:51 +0200

    Update Dashboard.js

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,50 +4,48 @@ import ApiKey from './apiKey/ApiKey';
 import Analytics from './analytics/Analytics';
 
 const Dashboard = () => {
-    return (
-        <Wrapper className="column">
-            <Content>
-                <StyledMain>
-                  <StyledCont>
-                    <ApiKey />
-                    <Analytics />
-                    <div className="section" />
-                  </StyledCont>
-                </StyledMain>
-            </Content>
-        </Wrapper>
-    );
+	return (
+		<Wrapper className="column">
+			<Content>
+				<StyledMain>
+					<StyledCont>
+						<ApiKey />
+						<Analytics />
+						<div className="section" />
+					</StyledCont>
+				</StyledMain>
+			</Content>
+		</Wrapper>
+	);
 };
 
 export default Dashboard;
 
-const Wrapper = styled.div`
-    background-color: #fff;
-`;
+const Wrapper = styled.div`background-color: #fff;`;
 
 const Content = styled.div`
-    flex-grow: 1;
-    overflow-y: auto;
-    padding: 25px;
+	flex-grow: 1;
+	overflow-y: auto;
 `;
 
 const StyledMain = styled.main`
-    min-width: 100%;
-    min-height: 100vh;
-    background-color: #fafafa;
-    display: flex;
-    justify-content: center;
+	min-width: 100%;
+	min-height: 100vh;
+	background-color: #fafafa;
+	display: flex;
+	justify-content: center;
 `;
 
 const StyledCont = styled.div`
-    width: 70%;
-    display: grid;
-    grid-row-gap: 3rem;
-    padding: 3rem 0;
+	width: 85%;
+	max-width: 1000px;
+	display: grid;
+	grid-row-gap: 3rem;
+	padding: 3rem 0;
 
-    .section {
-        background-color: #eeeeee;
-        min-height: 300px;
-        padding: 2rem;
-    }
+	.section {
+		background-color: #eeeeee;
+		min-height: 300px;
+		padding: 2rem;
+	}
 `;
