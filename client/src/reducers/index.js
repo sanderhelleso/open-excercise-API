8a6f9f41cbe5a1f175040d18e6c2b6ec5afaf0d2
Tarald Johansen <tarald.johansen@outlook.com>
2019-07-26 08:27:39 +0200

    Logout now clears entire state

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,18 @@
-import { combineReducers } from 'redux';
-import authReducer from './authReducer';
-import quotaReducer from './quotaReducer';
+import { combineReducers } from "redux";
+import authReducer from "./authReducer";
+import quotaReducer from "./quotaReducer";
 
-export default combineReducers({
-	auth: authReducer,
-	quota: quotaReducer
+const appReducer = combineReducers({
+    auth: authReducer,
+    quota: quotaReducer
 });
+
+export default (state, action) => {
+    // on logout, reset state
+    if (action.type === "LOGOUT") {
+        navigator.credentials.preventSilentAccess();
+        state = undefined;
+    }
+
+    return appReducer(state, action);
+};
