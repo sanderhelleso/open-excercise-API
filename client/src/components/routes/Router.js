d23941fa4259666fff3c762a8d1dff7ec87372e4
Tarald <tarald.johansen@outlook.com>
2019-08-04 22:01:03 +0200

    Fixed router issue

--- client/src/components/routes/Router.js
+++ client/src/components/routes/Router.js
@@ -1,33 +1,50 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import Register from '../register/Register';
-import SideMenu from '../sidemenu/SideMenu';
-import SocketTest from '../socket-test/SocketTest';
-import Login from '../login/Login';
-import { connect } from 'react-redux';
-import Dashboard from '../dashboard/Dashboard';
-import Docs from '../docs/Docs';
-import Payment from '../payment/Payment';
+import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import Register from "../register/Register";
+import SideMenu from "../sidemenu/SideMenu";
+import Login from "../login/Login";
+import { connect } from "react-redux";
+import Dashboard from "../dashboard/Dashboard";
+import Docs from "../docs/Docs";
+import Payment from "../payment/Payment";
 
 const Router = ({ isAuthenticated }) => {
-	return (
-		<BrowserRouter>
-			<Route path="/*" component={SideMenu} />
-			<Switch>
-				<Route exact path="/" component={isAuthenticated ? Dashboard : Login} />
-				<Route exact path="/login" component={isAuthenticated ? () => <Redirect to="/" /> : Login} />
-				<Route exact path="/socket-test" component={SocketTest} />
-				<Route exact path="/register" component={isAuthenticated ? () => <Redirect to="/" /> : Register} />
-				<Route exact path="/documentation" component={Docs} />
-				<Route exact path="/plans" component={Payment} />
-			</Switch>
-		</BrowserRouter>
-	);
+    return (
+        <BrowserRouter>
+            <Route path="/*" component={SideMenu} />
+            <Switch>
+                <Route
+                    exact
+                    path="/"
+                    component={isAuthenticated ? Dashboard : Login}
+                />
+                <Route
+                    exact
+                    path="/login"
+                    component={
+                        isAuthenticated ? () => <Redirect to="/" /> : Login
+                    }
+                />
+                <Route
+                    exact
+                    path="/register"
+                    component={
+                        isAuthenticated ? () => <Redirect to="/" /> : Register
+                    }
+                />
+                <Route exact path="/documentation" component={Docs} />
+                <Route exact path="/plans" component={Payment} />
+            </Switch>
+        </BrowserRouter>
+    );
 };
 
 const mapStateToProps = ({ auth }) => {
-	const { isAuthenticated } = auth;
-	return { isAuthenticated };
+    const { isAuthenticated } = auth;
+    return { isAuthenticated };
 };
 
-export default connect(mapStateToProps, null)(Router);
+export default connect(
+    mapStateToProps,
+    null
+)(Router);
