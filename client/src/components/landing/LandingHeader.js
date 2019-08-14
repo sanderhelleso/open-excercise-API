ab4172101429aad90febee61522e4558ad5a9ae9
Sanderhelleso <sanderhelleso@hotmail.com>
2019-08-14 22:12:02 +0200

    create_landing_page: updated image position

--- client/src/components/landing/LandingHeader.js
+++ client/src/components/landing/LandingHeader.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 import ButtonV2 from '../common/ButtonV2';
+import { withRouter } from 'react-router-dom';
+import { fadeInPure } from '../../lib/keyframes';
 
-const LandingHeader = () => {
+const LandingHeader = ({ history }) => {
 	return (
 		<StyledSection>
-			<div>
-				<StyledInner>
-					<h1>Open Excercise API</h1>
-					<StyledBtnCont>
-						<ButtonV2 text="Get Started" />
-						<ButtonV2 text="Documentation" flat={true} />
-					</StyledBtnCont>
-				</StyledInner>
-			</div>
+			<StyledInner>
+				<h1>Open Excercise API</h1>
+				<p>
+					Take your workout & fitness apps to the next level with our excercise data.
+					<br />
+					Get started for free today and recieve <span>10.000</span> monthly requests right now!
+				</p>
+				<StyledBtnCont>
+					<ButtonV2 text="Get Started" onClick={() => history.push('/register')} />
+					<ButtonV2 text="Documentation" flat={true} onClick={() => history.push('/documentation')} />
+				</StyledBtnCont>
+			</StyledInner>
 			<div>
 				<img src={`${process.env.PUBLIC_URL}/img/landing.png`} />
 			</div>
@@ -21,24 +26,20 @@ const LandingHeader = () => {
 	);
 };
 
-export default LandingHeader;
+export default withRouter(LandingHeader);
 
 const StyledSection = styled.section`
+	max-width: 70%;
 	position: relative;
 	display: flex;
 	align-content: center;
-	justify-content: center;
-
-	div {
-		min-height: 100%;
-		min-width: 200px;
-		position: relative;
-	}
+	min-height: 100vh;
+	margin: 0 auto;
 
 	img {
 		width: 800px;
 		height: auto;
-		margin-top: 5rem;
+		margin-top: 8rem;
 	}
 `;
 
@@ -46,12 +47,19 @@ const StyledInner = styled.div`
 	display: flex;
 	justify-content: center;
 	flex-direction: column;
-	margin-left: 3rem;
+	margin-bottom: 10rem;
+	position: relative;
+	min-width: 500px;
+	animation: ${fadeInPure} 0.7s ease forwards;
 
 	h1 {
-		font-size: 3rem;
+		font-size: 3.5rem;
 		font-weight: 800;
 		text-transform: uppercase;
+	}
+
+	span {
+		font-weight: 800;
 	}
 `;
 
