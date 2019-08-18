6b8d60f2a114b1f919f680d5834c815dd391608e
Sanderhelleso <sanderhelleso@hotmail.com>
2019-08-18 13:16:51 +0200

    add_error_handling_to_form: added validator

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -6,6 +6,7 @@ import InputV2 from '../common/InputV2';
 import ButtonV2 from '../common/ButtonV2';
 import { ArrowRight } from 'react-feather';
 import { fadeInPure } from '../../lib/keyframes';
+import { isEmptyObj, isPassword, isEmail, isName } from '../../lib/validators';
 
 const inputs = [
 	{
@@ -37,11 +38,11 @@ const inputs = [
 	},
 	{
 		type: 'password',
-		name: 'password_confirm',
+		name: 'confirmPassword',
 		placeholder: 'Confirm Password',
 		label: {
 			text: 'Confirm Password',
-			htmlForm: 'password_confirm'
+			htmlForm: 'confirmPassword'
 		}
 	}
 ];
@@ -50,8 +51,11 @@ const Register = () => {
 	const [ state, updateState ] = useReducer((state, newState) => ({ ...state, ...newState }), {
 		name: '',
 		email: '',
-		password: ''
+		password: '',
+		confirmPassword: ''
 	});
+
+	const { password, confirmPassword, email, name } = state;
 
 	const handleChange = ({ target: { name, value } }) => {
 		updateState({ [name]: value });
@@ -78,10 +82,20 @@ const Register = () => {
 		}
 	};
 
+	const isValidForm = () => {
+		return (
+			isEmptyObj(state) ||
+			!isPassword(password) ||
+			!isEmail(email) ||
+			!isName(name) ||
+			password !== confirmPassword
+		);
+	};
+
 	return (
 		<StyledForm onSubmit={handleSubmit}>
 			{renderInputs()}
-			<ButtonV2 text="Continue" icon={<ArrowRight />} />
+			<ButtonV2 text="Continue" icon={<ArrowRight />} disabled={isValidForm()} />
 		</StyledForm>
 	);
 };
